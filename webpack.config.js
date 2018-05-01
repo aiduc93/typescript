8d36ab2c52358013389170cd02ce9f6aa925d177
aiduc93 <aiduc93@gmail.com>
2018-05-01 16:04:51 +0700

    added project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'app.ts'),
+    output: {
+        filename: 'app.js',
+        path: __dirname
+    },
+    devServer: {
+        inline:true,
+        port: 3000
+      },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+};
